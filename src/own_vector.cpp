3c008ff13aae951dd877bd8c6e924e5d6eeace53
Corelli <46171138+Marco153@users.noreply.github.com>
2024-09-30 15:55:22 -0300

    a

--- src/own_vector.cpp
+++ src/own_vector.cpp
@@ -1,3 +1,4 @@
+
 namespace own_std
 {
 
@@ -134,6 +135,22 @@ namespace own_std
 			memcpy(buffer, other.data_, other.len);
 			data_ = buffer;
 			len = other.len;
+		}
+		void operator =(string &src)
+		{
+			unsigned int l = src.len;
+			char* buffer = (char*)__lang_globals.alloc(__lang_globals.data, l);
+			memcpy(buffer, src.data_, l);
+			data_ = buffer;
+			len = l;
+		}
+		void operator =(const char* src)
+		{
+			unsigned int l = strlen(src);
+			char* buffer = (char*)__lang_globals.alloc(__lang_globals.data, l);
+			memcpy(buffer, src, strlen(src));
+			data_ = buffer;
+			len = l;
 		}
 		string(const char* src)
 		{
@@ -343,7 +360,7 @@ namespace own_std
 		string operator +(string other)
 		{
 			// use concat_in_place instead
-			ASSERT(0);
+			//ASSERT(0);
 			return concat(other);
 		}
 		char& operator [](int idx)
@@ -594,3 +611,5 @@ namespace own_std
 		}
 	};
 }
+
+
